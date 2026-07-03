323c8675b8de3fdcc35484baab82425c0abf9a0b
SamiErenBayhan <155374516+SamiErenBayhan@users.noreply.github.com>
2026-07-04 00:47:56 +0300

    Skor Sistemi ve Hızlanma eklendi

--- ball.gd
+++ ball.gd
@@ -1,16 +1,39 @@
 extends CharacterBody2D
 
-var speed: float = 500.0
+var speed: float = 400.0
 var direction: Vector2 = Vector2.ZERO
+const START_SPEED: float = 400.0
+var is_resetting: bool = false
+const MAX_SPEED: float = 1000.0
 
 func _ready() -> void:
-	var random_x = randf_range(-1, 1)
-	var random_y = randf_range(-1, 1)#sağ veya sola herhangi bi yöne gitmesi için değer verdiriyoruz
-	direction = Vector2(random_x, random_y).normalized()#değer sapıtmasın yanlış hesaplar yapmasın diye bu kodu kullanıyoruz. Godot onu kendi ayarlıyor.
-
-func _physics_process(delta: float) -> void:
+	start_random_movement()
+	
+func _physics_process(delta):
 	var collision = move_and_collide(direction * speed * delta)
+	
 	if collision:
 		direction = direction.bounce(collision.get_normal())
+		if collision.get_collider().name.contains("LeftPlayer") or collision.get_collider().name.contains("RightPlayer"):
+			speed = clamp(speed * 1.05, START_SPEED, MAX_SPEED )
+			
 		
+func reset_ball() -> void:
+	if is_resetting:
+		return
+	is_resetting = true	
 	
+	global_position = Vector2(-8, 24)
+	velocity = Vector2.ZERO
+	speed = 0.0
+	
+	await get_tree().create_timer(1.0).timeout
+	
+	speed = START_SPEED
+	start_random_movement()
+	is_resetting = false
+
+func start_random_movement():
+	var random_x = -1.0 if randf() < 0.5 else 1.0
+	var random_y = randf_range(-0.5, 0.5)
+	direction = Vector2(random_x, random_y).normalized()
--- main.gd
+++ main.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
+var left_score = 0
+var right_score = 0
+@export var ball_node: CharacterBody2D
+
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -8,3 +11,21 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_right_score_area_body_entered(body: Node2D) -> void:
+	if body.name.contains("Ball"):
+		right_score += 1
+		print(right_score)
+		$RightScoreArea/Label.text = str(right_score)
+		if is_instance_valid(ball_node):
+			ball_node.reset_ball()
+			
+
+func _on_left_score_area_body_entered(body: Node2D) -> void:
+	if body.name.contains("Ball"):
+		left_score += 1
+		print(left_score)
+		$LeftScoreArea/Label.text = str(left_score)
+		if is_instance_valid(ball_node):
+			ball_node.reset_ball()
